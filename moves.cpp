c04cdeeb21c3705fb3073266f3c58820961143fe
Matthew Dippel <mdip226@gmail.com>
2021-09-30 10:40:15 -0400

    dir masks for edges

--- moves.cpp
+++ moves.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <iostream>
 #include <bitset>
+#include "debug.h"
 
 uint64_t get_legal_moves(uint64_t player, uint64_t opponent) {
     uint64_t full = player | opponent;
@@ -11,6 +12,7 @@ uint64_t get_legal_moves(uint64_t player, uint64_t opponent) {
     int cols = 8;
     for (int i = 0; i < 8; ++i)
     {
+        // std::cout << "C " << i << std::endl;
         int shift = 1;
         if (i != 2 && i != 6)
         {
@@ -28,19 +30,64 @@ uint64_t get_legal_moves(uint64_t player, uint64_t opponent) {
         {
             // i think need direction masks
             candidates = opponent & (player >> shift);
+            switch (i)
+            {
+            case 0:
+                candidates &= (~SOUTH);
+                break;
+            case 1:
+                candidates &= (~(SOUTH|EAST));
+                break;
+            case 2:
+                candidates &= (~EAST);
+                break;
+            case 3:
+                candidates &= (~(SOUTH|WEST));
+                break;
+            case 4:
+                candidates &= (~NORTH);
+                break;
+            case 5:
+                candidates &= (~(NORTH|WEST));
+                break;
+            case 6:
+                candidates &= (~WEST);
+                break;
+            case 7:
+                candidates &= (~(NORTH|EAST));
+                break;
+            default:
+                break;
+            }
+            // std::cout << "C candidates:" << std::endl;
+            // print_uint(candidates);
             while (candidates != 0)
             {
+                // std::cout << "C (candidates>>shift):" << std::endl;
+                // print_uint(candidates>>shift);
                 moves |= empty & (candidates >> shift);
+                // std::cout << "C moves:" << std::endl;
+                // print_uint(moves);
                 candidates = opponent & (candidates >> shift);
+                // std::cout << "C candidates:" << std::endl;
+                // print_uint(candidates);
             }
         }
         else
         {
             candidates = opponent & (player << shift);
+            // std::cout << "C candidates:" << std::endl;
+            // print_uint(candidates);
             while (candidates != 0)
             {
+                // std::cout << "C (candidates<<shift):" << std::endl;
+                // print_uint(candidates<<shift);
                 moves |= empty & (candidates << shift);
+                // std::cout << "C moves:" << std::endl;
+                // print_uint(moves);
                 candidates = opponent & (candidates << shift);
+                // std::cout << "C candidates:" << std::endl;
+                // print_uint(candidates);
             }
         }
         // std::cout << std::bitset<64>(moves).to_string() << std::endl;

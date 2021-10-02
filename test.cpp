e30e7bb44a7cc3f23c1c35ad38b5efa28fba321d
Matthew Dippel <mdip226@gmail.com>
2021-10-02 12:48:55 -0400

    testbed

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdint>
+#include <bitset>
+#include <tuple>
+#include "debug.h"
+#include "moves.h"
+#include "actions.h"
+#include "utils.h"
+using namespace std;
+
+#define AI 0
+#define OPPONENT 1
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout << "hi" << endl;
+    return 0;
+}

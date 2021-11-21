c02abfd09109127b268da48ae88ad763581713ab
Matthew Dippel <mdip226@gmail.com>
2021-11-21 11:19:55 -0500

    fixed!

--- actions.h
+++ actions.h
@@ -10,7 +10,7 @@
 u64 _make_move(u64 move, u64 board);
 
 // returns a tuple in the form tuple<player, opponent>
-std::tuple<u64, u64> play(u64 move, u64 player, u64 opponent, bool isAI);
+std::tuple<u64, u64> play(u64 move, u64 player, u64 opponent, bool isSelf);
 
 bool is_end(u64 player, u64 opponent);
 
--- test.cpp
+++ test.cpp
@@ -17,14 +17,23 @@ int main(int argc, char const *argv[])
 {
     /* code */
     cout << "hi" << endl;
-    u64 white = 0x1f3f010301000000;
-    u64 black = 0x20001e1c18040000;
-    tuple<u64, u64> boardBlack = make_tuple(black, white);
-    tuple<u64, u64> boardWhite = make_tuple(white, black);
+    // u64 white = 0x1f3f010301000000;
+    // u64 black = 0x20001e1c18040000;
+    u64 black = 0x001254386e385400;
+    u64 white = 0x1180000081000092;
+    print_uint(black);
+    print_uint(white);
     print_board(black, white);
-    print_legal_moves(black, white, true);
-    print_legal_moves(black, white, false);
-    // print_uint(0x8000000000000000);
+    u64 move = col_row_to_bit('d',5);
+    print_uint(move);
+    tuple<u64, u64> board = make_tuple(white, black);
+    board = play(move, white, black, false);
+    print_board(std::get<OPPONENT>(board),std::get<AI>(board));
+
+    // tuple<u64, u64> boardBlack = make_tuple(black, white);
+    // print_legal_moves(black, white, true);
+    // print_legal_moves(black, white, false);
+    // // print_uint(0x8000000000000000);
 
     return 0;
 }

877caf4e5522f9d5c824efb70d44472cce4be75b
Matthew Dippel <mdip226@gmail.com>
2021-11-21 16:32:49 -0500

    testbed

--- test.cpp
+++ test.cpp
@@ -8,32 +8,17 @@
 #include "moves.h"
 #include "actions.h"
 #include "utils.h"
-using namespace std;
+#include "minmax.h"
 
 #define AI 0
 #define OPPONENT 1
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    cout << "hi" << endl;
-    // u64 white = 0x1f3f010301000000;
-    // u64 black = 0x20001e1c18040000;
-    u64 black = 0x001254386e385400;
-    u64 white = 0x1180000081000092;
-    print_uint(black);
-    print_uint(white);
+    u64 white = init_white();
+    u64 black = init_black();
+    std::tuple<u64, u64> board = std::make_tuple(black, white);
     print_board(black, white);
-    u64 move = col_row_to_bit('d',5);
-    print_uint(move);
-    tuple<u64, u64> board = make_tuple(white, black);
-    board = play(move, white, black, false);
-    print_board(std::get<OPPONENT>(board),std::get<AI>(board));
-
-    // tuple<u64, u64> boardBlack = make_tuple(black, white);
-    // print_legal_moves(black, white, true);
-    // print_legal_moves(black, white, false);
-    // // print_uint(0x8000000000000000);
-
+    playAlphaBeta(board);
     return 0;
 }

b79c3ce2bff51533e5a0bb7724598a30e01d3c60
Matthew Dippel <mdip226@gmail.com>
2021-12-01 15:58:29 -0500

    bookkeeping

--- minmax.h
+++ minmax.h
@@ -1,11 +1,19 @@
 #if !defined(MINMAX_H)
 #define MINMAX_H
 
+#include <vector>
+
+u64 playMinimax(std::tuple<u64, u64> board);
+
+std::tuple<u64, int> _minimax(std::tuple<u64, u64> board, int ply, int player, int maxDepth);
+
 u64 playAlphaBeta(std::tuple<u64, u64> board);
 
-u64 _alphaBeta(std::tuple<u64, u64> board, int ply, int player, int alpha, int beta, int maxDepth);
+std::tuple<u64, int> _alphaBeta(std::tuple<u64, u64> board, int ply, int player, int alpha, int beta, int maxDepth);
 
 std::vector<u64> get_moves_list(u64 player, u64 opponent);
+
+int evaluate(std::tuple<u64, u64> board);
 
 
 #endif // MINMAX_H
